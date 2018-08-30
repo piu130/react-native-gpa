c37c095e30fffa33221d566cecdd9d0490690c9f
Ramon Boss <ramon.boss@outlook.com>
2018-08-30 10:03:56 +0200

    added support for android back button

--- src/views/pages/GoBackHeader.jsx
+++ src/views/pages/GoBackHeader.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
+import { BackHandler } from 'react-native-web'
 import { connect } from 'react-redux'
 import { Header } from 'react-native-elements'
 import { goBack } from 'connected-react-router'
 
 export class SemestersPageHeader extends Component {
+  componentDidMount () {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  componentWillUnmount () {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  onBackPress = () => {
+    this.props.goBack()
+    return true
+  }
+
   render () {
-    const { goBack, ...rest } = this.props
+    const { ...rest } = this.props
     return (
       <Header
         leftComponent={{
           icon: 'arrow-back',
-          onPress: goBack
+          onPress: this.onBackPress
         }}
         {...rest}
       />
