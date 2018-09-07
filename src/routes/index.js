678c9ccac280ab9c350b99cc2248f80a3167c208
Ramon Boss <ramon.boss@outlook.com>
2018-09-07 15:34:31 +0200

    removed unused comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,6 @@
 import { ListPage, EditPage } from '../views/pages'
 
 export default [
-  // {
-  //   path: '/(semesters)?',
-  //   component: SemestersPage,
-  //   exact: true
-  // },
   {
     path: '/:entity([^/]+s)/:id?',
     component: ListPage,
@@ -16,14 +11,4 @@ export default [
     component: EditPage,
     exact: true
   }
-  // {
-  //   path: '/semester/:action/:id?',
-  //   component: EditSemesterPage,
-  //   exact: true
-  // },
-  // {
-  //   path: '/subject/:action/:id?',
-  //   component: EditSemesterPage,
-  //   exact: true
-  // }
 ]
