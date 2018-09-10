816459351738fc7ef12eca36beaa1a8a03124c15
Ramon Boss <ramon.boss@outlook.com>
2018-09-10 11:21:35 +0200

    reordered exports

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,7 +1,8 @@
-export { default as AddButton } from './AddButton'
 export { default as EditPage } from './EditPage'
-export { default as GoBackHeader } from './GoBackHeader'
 export { default as ListPage } from './ListPage'
-export { default as MainView } from './MainView'
 export { default as SettingsPage } from './SettingsPage'
+
+export { default as AddButton } from './AddButton'
+export { default as GoBackHeader } from './GoBackHeader'
+export { default as MainView } from './MainView'
 export { default as TextField } from './TextField'
