a21031c9501ecacbde21c653ea1001de5ce0cf29
Ramon Boss <ramon.boss@outlook.com>
2018-09-10 09:57:48 +0200

    renamed selectors

--- src/state/ducks/entities/Semester/selectors.js
+++ src/state/ducks/entities/Semester/selectors.js
@@ -1,3 +1,3 @@
-const semesterSelector = state => state.entities.Semester
-export const getAll = state => semesterSelector(state).items
-export const byId = (state, id) => semesterSelector(state).itemsById[id]
+const selector = state => state.entities.Semester
+export const getAll = state => selector(state).items
+export const byId = (state, id) => selector(state).itemsById[id]
--- src/state/ducks/entities/Subject/selectors.js
+++ src/state/ducks/entities/Subject/selectors.js
@@ -1,3 +1,3 @@
-const subjectSelector = state => state.entities.Subject
-export const getAll = state => subjectSelector(state).items
-export const byId = (state, id) => subjectSelector(state).itemsById[id]
+const selector = state => state.entities.Subject
+export const getAll = state => selector(state).items
+export const byId = (state, id) => selector(state).itemsById[id]
