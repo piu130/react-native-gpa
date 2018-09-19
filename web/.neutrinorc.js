a11347c64c8d8c818d7e417721bdfdf93e95eb7f
Ramon Boss <ramon.boss@outlook.com>
2018-09-19 08:41:22 +0200

    moved .neutrinorc.js to web folder

--- web/.neutrinorc.js
+++ web/.neutrinorc.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 
-const nodeModulesDir = join(__dirname, 'node_modules')
+const nodeModulesDir = join(process.cwd(), 'node_modules')
 
 module.exports = {
   use: [
