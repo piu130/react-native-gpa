8355b627cb7248cf10af21d19e858cc85d89ff2a
Ramon Boss <ramon.boss@outlook.com>
2018-09-19 09:07:17 +0200

    direct export

--- src/state/ducks/ui/editing/reducers.js
+++ src/state/ducks/ui/editing/reducers.js
@@ -1,11 +1,9 @@
 import { CREATE, SET, REMOVE, CHANGE_PROPERTY } from './types'
 import { createReducer } from '../../../utils'
 
-const todoReducer = createReducer({})({
+export default createReducer({})({
   [CREATE]: (state, { payload }) => payload,
   [SET]: (state, { payload }) => payload,
   [REMOVE]: (state, action) => {},
   [CHANGE_PROPERTY]: (state, { payload }) => ({ ...state, [payload.property]: payload.value })
 })
-
-export default todoReducer
